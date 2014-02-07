b0b566279eec1c012565afca7bd52d21cf5c2739
Tor Hvalsøe <tor@itdude.dk>
2014-02-07 08:55:27 +0100

    Refactoring the module. Improved way of prototyping. Now using JSHint.

--- slideshare-viewer-html.js
+++ slideshare-viewer-html.js
@@ -1,71 +1,81 @@
-var SlideShareViewer = function(url, element, options){
-	// Jumps to a specific slide.
-	var jumpTo = function(slideNumber){
-		if (presentation == undefined || slideNumber >= presentation.total_slides || slideNumber < 0) return;
-		element.children[current].style.display = "none";
-		current = slideNumber;
-		element.children[current].style.display = "block";
-	}
-
+/* jshint strict: false */
+/* exported SlideShareViewer */
+/* jshint camelcase: false */
+var SlideShareViewer = function (url, element, options) {
 	// Sets the initial position and state variables
+	var _this = this;
 	var current = 0;
 	var presentation;
+	var callbackName = 'callback_' + Math.random().toString(36).substring(7);
+
+	// Defines a defauld transition
+	// var transition = options.transition;
+	this.transition = function (a, b) {
+		a.style.display = 'none';
+		b.style.display = 'block';
+	};
+
+	// Jumps to a specific slide.
+	this.jumpTo = function (slideNumber) {
+		if (presentation === undefined || slideNumber >= presentation.total_slides || slideNumber < 0) return;
+		this.transition(element.children[current], element.children[slideNumber]);
+		current = slideNumber;
+	};
 
 	// Defined the callback function for the json response
-	var callbackName = "callback_" + Math.random().toString(36).substring(7);
 	window[callbackName] = function(obj){
 		if (obj.error === true) {
-			element.innerHTML = "<p>Error loading from SlideShare.net</p>";
+			element.innerHTML = '<p>Error loading from SlideShare.net</p>';
 			return;
 		}
 		presentation = obj;
 		
 		// Build the html for the presentation
 		for(var i = 1; i <= presentation.total_slides; i++){
-			element.innerHTML = element.innerHTML + "<img style=\"display:none;\" src=\"" + presentation.slide_image_baseurl + i + presentation.slide_image_baseurl_suffix + "\" />";
+			element.innerHTML = element.innerHTML + '<img style="display:none;" src="' + presentation.slide_image_baseurl + i + presentation.slide_image_baseurl_suffix + '" />';
 		}
-		jumpTo(current);
+
+		_this.jumpTo(current);
 
 		// Triggers ready callback
-		if (options.readyCallback != undefined){
+		if (options.readyCallback !== undefined) {
 			options.readyCallback();
 		}
-	}
+	};
 
 	// Starts loading data from SlideShare.net
 	var script = document.createElement('script');
-	script.type= "text/javascript";
-	script.src= "http://www.slideshare.net/api/oembed/2?url=" + url + "&format=json&callback=window." + callbackName;
+	script.type= 'text/javascript';
+	script.src= 'http://www.slideshare.net/api/oembed/2?url=' + url + '&format=json&callback=window.' + callbackName;
 	document.getElementsByTagName('head')[0].appendChild(script);
 
-	// Defined the interface
-	return {
-		// Returns the title of the slide show
-		getTitle: function(){
-			return presentation.title;
-		},
-		// Returns the number of the current slide
-		currentSlide: function(){
-			return current;
-		},
+	// Returns the title of the slide show
+	this.getTitle = function(){
+		return presentation.title;
+	};
+	
+	// Returns the number of the current slide
+	this.currentSlide = function(){
+		return current;
+	};
 
-		// Goes to the specified slide number
-		jump: jumpTo,
-		// Goes to the next slide
-		next: function(){
-			jumpTo(current+1);
-		},
-		// Goes to the previous slide
-		prev: function(){
-			jumpTo(current-1);
-		},
-		// Goes to the first slide
-		first: function(){
-			jumpTo(0);
-		},
-		// Goes to the last slide
-		last: function(){
-			jumpTo(presentation.total_slides-1);
-		}
+	// Goes to the next slide
+	this.next = function(){
+		this.jumpTo(current+1);
+	};
+	
+	// Goes to the previous slide
+	this.prev = function(){
+		this.jumpTo(current-1);
+	};
+	
+	// Goes to the first slide
+	this.first = function(){
+		this.jumpTo(0);
+	};
+
+	// Goes to the last slide
+	this.last = function(){
+		this.jumpTo(presentation.total_slides-1);
 	};
 };
